33c0ab005e44d1f421eabf8e3e5c2bba9f57633c
janethrivera209 <riverarobledojaneth@gmail.com>
2026-06-22 09:50:18 -0600

    Corrige imports para despliegue en Render

--- gps/app.py
+++ gps/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from gps.configuracion import Configuracion
-from routes import registrar_rutas
+from gps.routes import registrar_rutas
 
 
 def crear_aplicacion(configuracion_prueba=None):
